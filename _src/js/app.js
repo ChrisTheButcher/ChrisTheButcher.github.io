fb1b11289cf2673f18aa19cc27a4e022a7d61fe5
christhebutcher <ccwijnia@gmail.com>
2017-09-17 14:31:48 +0200

    instagram

--- _src/js/app.js
+++ _src/js/app.js
@@ -1,55 +1,60 @@
-import {$select} from "./components/DomHelper";
-import {ToggleClass} from "./components/ToggleClass";
+import {$select, json, ToggleClass, inView} from "./components/DomHelper";
 import {Parallax} from "./components/Parallax";
+import {AsyncMedia} from "./components/AsyncMedia";
+import moment from "moment";
 
-new Parallax("header", "", "video, .bg")
-new Parallax("section", "", ".bg")
+new Parallax("header", "", "video, .bg");
+new Parallax("section", "", ".bg");
+new AsyncMedia("img, video, [style*='background-image']");
 
 const hashLinks = $select("[href*='#']");
 const inverted = $select("section[data-inverted='true']");
-const media = $select("img, video, [style*='background-image']");
 const body = document.body;
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+let activePage = "#about";
 
 body.classList.toggle("is-mobile", isMobile);
 
-function inView(element) {
-  const nav = document.querySelector(".nav").getBoundingClientRect();
-  const navHalf = nav.top + (nav.height / 2);
-  const section = element.getBoundingClientRect();
-  return (section.top < navHalf) && !(section.bottom < navHalf);
-};
-
-media.forEach(e => {
-  let saved, img;
-  let listerer = e.tagName === "VIDEO" ? "loadeddata" : "load";
-
-  if (e.matches("[style*='background-image']")) {
-    saved = e.style.backgroundImage;
-    e.style.backgroundImage = "";
-    img = document.createElement("img");
-    img.src = saved.slice(4, -1).replace(/"/g, "");
-    setTimeout(()=> { e.style.backgroundImage = saved }, 30)
-  }
-  else {
-    saved = e.src;    
-    e.src = "";
-    img = e;
-
-    if (e.tagName === "VIDEO") {
-      setTimeout(()=> { e.src = saved })    
-    }
-    else {
-      setTimeout(()=> { e.src = saved }, 3000) 
-    }
-  }
-
-  img.addEventListener(listerer, () => e.classList.add("loaded"))
-})
-
-hashLinks.forEach(e => e.addEventListener("click", e => body.classList.toggle("menu-open")));
-
-["scroll", "resize"].forEach(e => addEventListener(e, () => {
-  const themeInverted = inverted.map(x => inView(x)).indexOf(true) > -1;
+function updateTheme() {
+  const themeInverted = inverted.map(x => inView(x, 40)).indexOf(true) > -1;
   body.classList.toggle("theme-inverted", themeInverted)
-}));
+}
+
+function updateNav() {
+  const hash = window.location.hash;  
+  const showMenu = hash === "#menu";
+  if (!showMenu) activePage = hash || "#about";
+
+  body.classList.toggle("menu-open", showMenu);
+  document.querySelector('.nav').href = showMenu ? '#' : "#menu";
+  $select('#menu li a').forEach(e => {
+    e.classList.toggle("active", e.matches(`[href="${activePage}"]`))
+  })
+}
+
+["scroll", "resize", "load"].forEach(e => addEventListener(e, updateTheme));
+["hashchange", "load"].forEach(e => addEventListener(e, updateNav));
+
+window.getInstagram =()=> {
+  const token = '5222788.2177b6c.daf90d77db5441b0a707808c1ebfc6da';
+  const url = `https://api.instagram.com/v1/users/self/media/recent?access_token=${token}&count=9`;
+  const element = document.querySelector('.instagram');
+  let imagesTemplate = ""
+  json(url).then(data => {
+    data.data.forEach(post => {
+      const img = post.images.standard_resolution.url;
+      const time = moment(post.created_time*1000).format("MM/DD/YYYY");
+      imagesTemplate += 
+        `<li>
+          <a href="${post.link}" style="background-image: url(${img});">
+            <div class="caption">
+              <span class="user">@${post.user.username}</span>
+              <span class="meta">${time} — ❤${post.likes.count}</span>
+            </div>
+          </a>
+        </li>`;
+   })
+   element.innerHTML = `<ul>${imagesTemplate}</ul>`
+   new AsyncMedia(".instagram a");   
+  })  
+}
--- _src/js/components/DomHelper.js
+++ _src/js/components/DomHelper.js
@@ -1,1 +1,34 @@
+function toggleClass() {
+    const toggleData = JSON.parse(this.getAttribute('data-toggle-class'));
+    for (const key of Object.keys(toggleData)) {
+        $select(key).forEach(el=> el.classList.toggle(toggleData[key]))
+    }
+}
+
 export const $select = selector => [...document.querySelectorAll(selector)]
+
+export function json(url) {
+    return new Promise((resolve, reject) => {
+        const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);        
+        const callbackName = "cb_" + s4();
+        const script = document.createElement('script');    
+        window[callbackName] = data => {
+            delete window[callbackName];
+            resolve(data)
+        };
+        script.src = `${url}&callback=${callbackName}`;  
+        document.body.appendChild(script);
+        script.onload = script.remove();
+    })
+}
+
+export class ToggleClass {
+    constructor() {
+        $select('[data-toggle-class]').forEach(el=> el.addEventListener('click', toggleClass))
+    }
+}
+
+export function inView(element, offset = 0) {
+    const section = element.getBoundingClientRect();
+    return (section.top < 0 + offset) && !(section.bottom < 0 + offset);
+};
