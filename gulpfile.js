55cc367d2a315679e28dea04e39c1e0d90795636
christhebutcher <ccwijnia@gmail.com>
2017-09-23 18:07:44 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const sass = require('gulp-sass')
 const prefix = require('gulp-autoprefixer');
 const sync = require('browser-sync');
 const cp = require('child_process');
+const gutil = require('gulp-util');
+const ftp = require('gulp-ftp');
+const ftpConfig = require('./ftp-config');
 
 const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 const messages = {
@@ -29,16 +32,20 @@ const webpackConfig = {
     output: { filename: 'app.js' }
 }
 
-gulp.task('scripts', [], ()=> gulp.src('_src/js/app.js')
-    .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest('assets/js')))
+gulp.task('scripts', [], (cb)=> {
+    gulp.src('_src/js/app.js')
+        .pipe(webpack(webpackConfig))
+        .pipe(gulp.dest('assets/js'));
+
+})
+
 
 gulp.task('styles', ()=> gulp.src('_src/scss/app.scss')
     .pipe(sass().on('error', sass.logError))    
     .pipe(prefix(['last 2 versions'], { cascade: true }))    
     .pipe(gulp.dest('assets/css')))
 
-gulp.task('serve', ['styles', 'jekyll:build'], ()=> {
+gulp.task('serve', ['styles', 'build'], ()=> {
     gulp.start(['scripts'])
 
     sync.init({
@@ -51,18 +58,24 @@ gulp.task('serve', ['styles', 'jekyll:build'], ()=> {
 
 
 
-gulp.task('jekyll:build', done => {
+gulp.task('build', done => {
     sync.notify(messages.jekyllBuild);
     return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
         .on('close', done);
 });
 
-gulp.task('jekyll:rebuild', ['jekyll:build'], ()=> sync.reload());
+gulp.task('jrebuild', ['build'], ()=> sync.reload());
 
 
 gulp.task('watch', ['serve'], ()=> {
     gulp.watch('_src/scss/**/*.scss', ['styles']);
-    gulp.watch(['*.html', '_layouts/*', '_includes/*', '_posts/*', '_pages/*', '_drafts/*', 'assets/**/*'], ['jekyll:rebuild']);
+    gulp.watch(['*.html', '_layouts/*', '_includes/*', '_posts/*', '_pages/*', '_drafts/*', 'assets/**/*'], ['rebuild']);
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('_site/**/*')
+        .pipe(ftp(ftpConfig))
+        .pipe(gutil.noop());
 });
 
 gulp.task('default', ['watch'])
